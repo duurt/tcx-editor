3a38bc7ca2adf556c052fdc6b614369d68118730
Duurt <hoi@hoi.com>
2019-08-21 23:15:13 +0200

    Added delete course-point command

---
+++ Source/TcxEditor.Core/DeleteCoursePointCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcxEditor.Core.Exceptions;
+using TcxEditor.Core.Interfaces;
+
+namespace TcxEditor.Core
+{
+    public class DeleteCoursePointCommand : IDeleteCoursePointCommand
+    {
+        public DeleteCoursePointResponse Execute(DeleteCoursePointInput input)
+        {
+            if (input.Route == null)
+                throw new ArgumentNullException(nameof(input), nameof(input.Route));
+
+            if (!input.Route.CoursePoints.Any(p => p.TimeStamp == input.TimeStamp))
+                throw new TcxCoreException($"Cannot delete point with timestamp {input.TimeStamp}. Point not found.");
+
+            input.Route.CoursePoints.RemoveAll(p => p.TimeStamp == input.TimeStamp);
+
+            return
+                new DeleteCoursePointResponse
+                {
+                    Route = input.Route
+                };
+        }
+    }
+}
---
+++ Source/TcxEditor.Core/DeleteCoursePointInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcxEditor.Core.Entities;
+
+namespace TcxEditor.Core
+{
+    public class DeleteCoursePointInput
+    {
+        public Route Route { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
