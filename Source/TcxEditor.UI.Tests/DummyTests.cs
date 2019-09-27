242852473d4b06fe9e40cee68231389e78d4f7be
Duurt <hoi@hoi.com>
2019-09-27 23:02:17 +0200

    Added dummy test ui.tests

---
+++ Source/TcxEditor.UI.Tests/DummyTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcxEditor.UI.Tests
+{
+    public class DummyTests
+    {
+        [Test]
+        public void PlaceHolderTest()
+        {
+            Console.WriteLine(
+                "It seems that without tests in this assembly, the build fails :-( There will be UI tests soon!");
+        }
+    }
+}
