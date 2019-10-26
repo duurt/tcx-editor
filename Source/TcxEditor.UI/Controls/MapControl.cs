410c24a888bf463749e48ec3d37d8f390e289f82
Duurt <hoi@hoi.com>
2019-10-26 10:50:48 +0200

    Map is moving on selecting points. Plus some refactoring of MapControl

--- Source/TcxEditor.UI/Controls/MapControl.cs
+++ Source/TcxEditor.UI/Controls/MapControl.cs
@@ -15,18 +15,22 @@ namespace TcxEditor.UI
 {
     public partial class MapControl : UserControl
     {
+        private readonly GMapOverlay LAYER_ROUTE = new GMapOverlay("route");
+        private readonly GMapOverlay LAYER_POINTS = new GMapOverlay("points");
+        private readonly GMapOverlay LAYER_EDIT_POINTS = new GMapOverlay("editPoints");
+
         public event EventHandler<MapClickEventArgs> MapClickEvent;
         public event EventHandler<PointSelectEventArgs> CoursePointSelectEvent;
-        
+
         public MapControl()
         {
             InitializeComponent();
             GMaps.Instance.Mode = AccessMode.ServerOnly;
             gMapControl1.MapProvider = GoogleMapProvider.Instance;
-           
-            gMapControl1.Overlays.Add(new GMapOverlay("route"));
-            gMapControl1.Overlays.Add(new GMapOverlay("points"));
-            gMapControl1.Overlays.Add(new GMapOverlay("editPoints"));
+
+            gMapControl1.Overlays.Add(LAYER_ROUTE);
+            gMapControl1.Overlays.Add(LAYER_POINTS);
+            gMapControl1.Overlays.Add(LAYER_EDIT_POINTS);
 
             gMapControl1.MinZoom = 0;
             gMapControl1.MaxZoom = 100;
@@ -36,7 +40,7 @@ namespace TcxEditor.UI
             gMapControl1.OnMarkerClick += OnMarkerClick;
         }
 
-        public void SetPosition(string location)
+        public void SetLocation(string location)
         {
             gMapControl1.MapProvider = OpenCycleLandscapeMapProvider.Instance;
             gMapControl1.SetPositionByKeywords(location);
@@ -45,7 +49,7 @@ namespace TcxEditor.UI
 
         private void OnMarkerClick(GMapMarker item, MouseEventArgs e)
         {
-            if (!item.Overlay.Id.Equals("points"))
+            if (!item.Overlay.Id.Equals(LAYER_POINTS.Id))
                 return;
 
             CoursePointSelectEvent?.Invoke(
@@ -78,55 +82,56 @@ namespace TcxEditor.UI
                 "someName");
 
             routeOnMap.Stroke = new Pen(Color.Red, 3);
-            GMapOverlay routeLayer = gMapControl1.Overlays.First(o => o.Id.Equals("route"));
-            routeLayer.Routes.Clear();
-            routeLayer.Routes.Add(routeOnMap);
+            LAYER_ROUTE.Routes.Clear();
+            LAYER_ROUTE.Routes.Add(routeOnMap);
 
-            GMapOverlay markerOverlay = gMapControl1.Overlays.First(o => o.Id.Equals("points"));
             foreach (var point in openedRoute.CoursePoints)
             {
                 GMarkerGoogle marker = new GMarkerGoogle(
                     new PointLatLng(point.Lattitude, point.Longitude),
                     new Bitmap(new MemoryStream(Convert.FromBase64String("iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAMAAAAMCGV4AAAASFBMVEX///9ISEgGBgbBwcH39/dpaWmYmJjR0dH09PSBgYGJiYnV1dV4eHh+fn7FxcVSUlJgYGDl5eWjo6Pc3Nx5eXk4ODhfX18nJycGtpgYAAAAcklEQVQImUWPWxaAIAhER83MxNKe+99pKFTzAdzDAQaA5WKmHB1Egy9j2MN4+aHhtEaOtwfiOnFFM17NBCwFv8qCvLXCWtPSlkGpd87OiUC1s+lc6e0Lp0PnlXle9wvzfrlvWXJf/TWJv89/Ef/63yH/PeORA/kIj+u1AAAAAElFTkSuQmCC"))));
 
-                markerOverlay.Markers.Add(marker);
+                LAYER_POINTS.Markers.Add(marker);
                 marker.ToolTipText = $"{point.Type}\n{point.Notes}";
                 marker.Tag = point.TimeStamp;
             }
         }
 
-        internal void ShowPointToEdit(TrackPoint point)
-        {
-            ClearEditMarkers();
-
-            GMapOverlay editOverlay = gMapControl1.Overlays.First(o => o.Id.Equals("editPoints"));
-            editOverlay.Markers.Add(
-                new GMarkerGoogle(
-                    new PointLatLng(point.Lattitude, point.Longitude),
-                    GMarkerGoogleType.blue){ Tag = point.TimeStamp });
-        }
-
         private void ClearCueMarkers()
         {
-            GMapOverlay editOverlay = gMapControl1.Overlays.First(o => o.Id.Equals("points"));
-            editOverlay.Markers.Clear();
+            LAYER_POINTS.Clear();
+        }
+
+        internal void ShowPointToEdit(TrackPoint point)
+        {
+            ShowSingleMarker(point, GMarkerGoogleType.blue, LAYER_EDIT_POINTS);
+        }
+
+        internal void SetEditCoursePointMarker(TrackPoint point)
+        {
+            ShowSingleMarker(point, GMarkerGoogleType.arrow, LAYER_EDIT_POINTS);
+        }
+
+        private void ShowSingleMarker(TrackPoint point, GMarkerGoogleType markerType, GMapOverlay layer)
+        {
+            ClearEditMarkers();
+            layer.Markers.Add(
+                new GMarkerGoogle(
+                    new PointLatLng(point.Lattitude, point.Longitude),
+                    markerType)
+                { Tag = point.TimeStamp });
+
+            SetMapCenterPosition(point);
         }
 
         private void ClearEditMarkers()
         {
-            GMapOverlay editOverlay = gMapControl1.Overlays.First(o => o.Id.Equals("editPoints"));
-            editOverlay.Markers.Clear();
+            LAYER_EDIT_POINTS.Clear();
         }
 
-        internal void SetEditCoursePointMarker(Position position)
+        private void SetMapCenterPosition(Position point)
         {
-            ClearEditMarkers();
-
-            GMapOverlay editOverlay = gMapControl1.Overlays.First(o => o.Id.Equals("editPoints"));
-            editOverlay.Markers.Add(
-                new GMarkerGoogle(
-                    new PointLatLng(position.Lattitude, position.Longitude),
-                    GMarkerGoogleType.arrow));
+            gMapControl1.Position = new PointLatLng(point.Lattitude, point.Longitude);
         }
     }
 
